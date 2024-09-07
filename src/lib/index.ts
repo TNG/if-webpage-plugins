241003ebe43c70443a04e2658c2a70ca94c399cd
alexzurbonsen <alexander.zur.bonsen@tngtech.com>
2024-09-08 01:55:42 +0200

    add shell exec plugin for execution of arbitrary shell commands
    Signed-off-by: alexzurbonsen <alexander.zur.bonsen@tngtech.com>

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 export {Co2js} from './co2js/index';
 export {GreenHosting} from './green-hosting/index';
 export {MeasureWebpage} from './measure-webpage/index';
+export {ShellExecCommand} from './shell-exec-command/index';
 export {TimerStart, TimerStop} from './timer/index';
-export {Identity, Rename} from './utils/index';
