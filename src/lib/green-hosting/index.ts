fcc85b160828449da3dbfc0d0685424d035e105d
alexzurbonsen <alexander.zur.bonsen@tngtech.com>
2024-10-26 02:18:59 +0200

    chore: refactor green hosting plugin to comply with IF 0.7.1 interface
    Signed-off-by: alexzurbonsen <alexander.zur.bonsen@tngtech.com>

--- src/lib/green-hosting/index.ts
+++ src/lib/green-hosting/index.ts
@@ -4,34 +4,33 @@ import {z} from 'zod';
 
 import {allDefined, validate} from '../../util/validations';
 
-import {PluginInterface} from '../../interfaces';
-import {PluginParams} from '../../types/common';
+import {PluginFactory} from '@grnsft/if-core/interfaces';
+import {PluginParams, ConfigParams} from '@grnsft/if-core/types';
 
-export const GreenHosting = (): PluginInterface => {
-  const metadata = {
-    kind: 'execute',
-  };
-
-  /**
-   * Executes the green hosting check for given url.
-   */
-  const execute = async (inputs: PluginParams[]): Promise<PluginParams[]> => {
-    return await Promise.all(
-      inputs.map(async input => {
-        const validatedInput = validateSingleInput(input);
-        const domain = getDomain(validatedInput.url);
-        return {
-          ...input,
-          'green-web-host': domain ? await hosting.check(domain) : undefined,
-        };
-      })
-    );
-  };
-
-  /**
-   * Validates the input parameters of the green hosting model.
-   */
-  const validateSingleInput = (input: PluginParams) => {
+export const GreenHosting = PluginFactory({
+  metadata: {
+    inputs: {
+      url: {
+        description:
+          'The url of the web page whose domain is checked for green hosting.',
+        unit: 'none',
+        'aggregation-method': {time: 'none', component: 'none'},
+      },
+    },
+    outputs: {
+      'green-web-host': {
+        description:
+          'True, false or undefined, indicating that the domain is or is not hosted green. If the return value is undefined, no domain could be extracted from the provided url.',
+        unit: 'none',
+        'aggregation-method': {time: 'none', component: 'none'},
+      },
+    },
+  },
+  inputValidation: (
+    input: PluginParams,
+    _config: ConfigParams,
+    _index: number | undefined
+  ) => {
     const schema = z
       .object({
         url: z.string(),
@@ -39,10 +38,29 @@ export const GreenHosting = (): PluginInterface => {
       .refine(allDefined, {message: '`url` must be provided.'});
 
     return validate<z.infer<typeof schema>>(schema, input);
-  };
-
-  return {
-    metadata,
-    execute,
-  };
-};
+  },
+  implementation: async (inputs: PluginParams[], _config: ConfigParams) => {
+    return await Promise.all(
+      inputs.map(async input => {
+        const domain = getDomain(input.url);
+        if (domain === null) {
+          console.warn(
+            `GreenHosting: Could not extract domain from url ${input.url}`
+          );
+        }
+        // generate timestamp and duration if they do not exist in input
+        if (!('timestamp' in input) && !('duration' in input)) {
+          input = {
+            timestamp: new Date().toISOString(),
+            duration: 0, // We are getting information from an API. No need to know how long we waited.
+            ...input,
+          };
+        }
+        return {
+          ...input,
+          'green-web-host': domain ? await hosting.check(domain) : undefined,
+        };
+      })
+    );
+  },
+});
