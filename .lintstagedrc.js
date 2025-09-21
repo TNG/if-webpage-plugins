1e3bef344a31904a9d684ed5eecb2a8286f74eec
Alexander zur Bonsen <alexander.zur.bonsen@tngtech.com>
2025-09-22 01:23:35 +0200

    fix: fix broken build path and add package validation test
    Signed-off-by: Alexander zur Bonsen <alexander.zur.bonsen@tngtech.com>

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   '*.ts': [
-    'gts lint',
-    'npm run test:pre-commit',
+    'npm run lint',
+    'npm run test:unit:ci',
   ],
-  'package.json': 'fixpack',
+  '{src/**/*.ts,vitest.config.ts}': () => 'npm run typecheck', // use function syntax to avoid file paths being passed to tsc. There were occasions when that broke the command.
+  'package.json': 'npm run fix:package',
 }
